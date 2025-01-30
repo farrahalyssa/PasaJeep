3dcfcff8523766532fb932fcc58809753b379805
farrahalyssa <idcfarrah@gmail.com>
2025-01-30 14:01:03 -0500

    initial commit for this project

---
+++ client/screens/auth/intro.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const intro = () => {
+  return (
+   <View>
+        <Text>PasaJeep</Text>
+        <Text>Pasajeep tracks your feet in real-time and ensures smooth operation with automated preventive maintenance alerts.</Text>
+        <TouchableOpacity><Text>Get Started</Text></TouchableOpacity> 
+   </View>
+  )
+}
+
+export default intro
